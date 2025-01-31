5dafed20e2131a55c6c59a3f888ef70b1f74f693
Gregory Pereverzev <boot762@gmail.com>
2025-01-31 20:21:18 +0300

    update CORS

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,60 +1,75 @@
-const config = require("config")
-const Koa = require("koa")
+const config = require("config");
+const Koa = require("koa");
+const app = new Koa();
 
-const app = new Koa()
+app.proxy = true;
+app.keys = [config.get("secret")];
 
-app.proxy = true
+require("../schemas")(app);
 
-app.keys = [config.get("secret")]
+const responseTime = require("koa-response-time");
+const helmet = require("koa-helmet");
+const logger = require("koa-logger");
+const xRequestId = require("koa-x-request-id");
+const camelizeMiddleware = require("../middleware/camelize-middleware");
+const error = require("../middleware/error-middleware");
+const cors = require("@koa/cors"); // Используем более актуальный пакет CORS
+const jwt = require("../middleware/jwt-middleware");
+const bodyParser = require("koa-bodyparser");
+const pagerMiddleware = require("../middleware/pager-middleware");
+const userMiddleware = require("../middleware/user-middleware");
+const routes = require("../routes");
 
-require("../schemas")(app)
+// Порядок middleware критически важен!
+app.use(responseTime());
+app.use(xRequestId({ inject: true }, app));
+app.use(helmet());
 
-const responseTime = require("koa-response-time")
-const helmet = require("koa-helmet")
-const logger = require("koa-logger")
-const xRequestId = require("koa-x-request-id")
-const camelizeMiddleware = require("../middleware/camelize-middleware")
-const error = require("../middleware/error-middleware")
-const cors = require("kcors")
-const jwt = require("../middleware/jwt-middleware")
-const bodyParser = require("koa-bodyparser")
-const pagerMiddleware = require("../middleware/pager-middleware")
-const userMiddleware = require("../middleware/user-middleware")
-const routes = require("../routes")
-
-
-
-app.use(responseTime())
-app.use(xRequestId({ inject: true }, app))
-app.use(logger())
-app.use(helmet())
-
+// Настройки CORS должны быть одними из первых middleware
 app.use(
   cors({
-    // origin: "https://blog.greg-p.keenetic.pro/",
-    origin: "*",
+    origin: [
+      "https://blog.greg-p.keenetic.pro",
+      "http://localhost:5400"
+    ],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Authorization", "Content-Type"],
     exposeHeaders: ["Authorization"],
     credentials: true,
-    allowMethods: ["GET", "PUT", "POST", "DELETE"],
-    allowHeaders: ["Authorization", "Content-Type"],
-    keepHeadersOnError: true,
-  }),
-)
+    maxAge: 86400, // Кэшировать предварительные запросы на 24 часа
+  })
+);
 
-app.use(camelizeMiddleware)
+app.use(logger());
+app.use(camelizeMiddleware);
+app.use(error);
 
-app.use(error)
-app.use(jwt)
+// Явная обработка OPTIONS-запросов
+app.use(async (ctx, next) => {
+  if (ctx.method === "OPTIONS") {
+    ctx.status = 204;
+    ctx.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    ctx.set("Access-Control-Allow-Headers", "Authorization, Content-Type");
+    return;
+  }
+  await next();
+});
+
+app.use(jwt);
 app.use(
   bodyParser({
     enableTypes: ["json"],
-  }),
-)
+  })
+);
+app.use(userMiddleware);
+app.use(pagerMiddleware);
 
-app.use(userMiddleware)
-app.use(pagerMiddleware)
+// Маршруты
+app.use(routes.routes());
+app.use(routes.allowedMethods({
+  throw: true, // Бросать ошибки для неразрешенных методов
+  notImplemented: () => new Error("Not implemented"),
+  methodNotAllowed: () => new Error("Method not allowed")
+}));
 
-app.use(routes.routes())
-app.use(routes.allowedMethods())
-
-module.exports = app
+module.exports = app;
